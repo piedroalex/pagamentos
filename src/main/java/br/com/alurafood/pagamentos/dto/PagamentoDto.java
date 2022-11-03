e97116a0522466de7c3cda1d41f6362a65c99fc5
Pedro Alex <piedroalex@gmail.com>
2022-11-03 16:38:53 -0300

    Repository e dto de pagamento criados.

---
+++ src/main/java/br/com/alurafood/pagamentos/dto/PagamentoDto.java
@@ -0,0 +1,22 @@
+package br.com.alurafood.pagamentos.dto;
+
+import java.math.BigDecimal;
+
+import br.com.alurafood.pagamentos.model.Status;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class PagamentoDto {
+	
+	private Long id;
+	private BigDecimal valor;
+	private String nome;
+	private String numero;
+	private String expiracao;
+	private String codigo;
+	private Status status;
+	private Long periodoId;
+	private Long formaDePagamentoId;
+	
+}
